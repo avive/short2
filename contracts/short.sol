f117cf79b4e69c61f185973321bbb99ccfaab1b5
Aviv Eyal <aviveyal07@gmail.com>
2019-04-14 17:14:16 +0300

    Minor edits

--- contracts/short.sol
+++ contracts/short.sol
@@ -7,6 +7,7 @@
                             |___/
   Shirley Shor * Art Token * Smart Contact * Team Human
 */
+
 pragma solidity >=0.5.0 <0.6.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
