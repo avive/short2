66d5af22281ab6d81c40a30132a39c997e564010
Aviv Eyal <aviveyal07@gmail.com>
2019-04-27 11:52:31 +0300

    Styling

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([
         { from: "./src/index.html", to: "index.html" },
-        { from: "./src/artregistry.html", to: "artregistry.html" }
+        { from: "./src/artregistry.html", to: "artregistry.html" },
+        { from: "./src/teamhuman.html", to: "teamhuman.html" }
     ]),
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
