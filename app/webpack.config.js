56a7deedaf48bd7335fae7641880dde8d89d630c
Aviv Eyal <aviveyal07@gmail.com>
2019-05-25 21:04:48 +0300

    Update flow

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -16,7 +16,13 @@ module.exports = {
         { from: "./src/teamhuman.html", to: "teamhuman.html" },
         { from: "./src/intro.html", to: "intro.html" },
         { from: "./src/poster.html", to: "poster.html" },
-        { from: "./src/poster1.html", to: "poster1.html" }
+        { from: "./src/poster1.html", to: "poster1.html" },
+        { from: "./src/poster2.html", to: "poster2.html" },
+        { from: "./src/poster3.html", to: "poster3.html" },
+        { from: "./src/poster4.html", to: "poster4.html" },
+        { from: "./src/poster5.html", to: "poster5.html" },
+        { from: "./src/poster6.html", to: "poster6.html" },
+        { from: "./src/poster7.html", to: "poster7.html" }
     ]),
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
